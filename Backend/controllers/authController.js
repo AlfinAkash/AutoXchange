be64a95db8eb9dcfa260b841c742caa560f5cc0c
AlfinAkash <alfinakash2001@gmail.com>
2026-02-06 20:56:13 +0530

    fix: updated samesite none

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -57,7 +57,8 @@ const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
 
